d956299fc6bf56ad8dd0e4fdb46fed3428cfe0ea
Saima Imran <smalldeeds1234@gmail.com>
2025-01-18 12:19:58 +0500

    Add files via upload

---
+++ Javascript ch# 3&4/app.js
@@ -0,0 +1,65 @@
+///////Chapter 3////////
+//1.Declare a variable called age & assign to it your age. Show your age in an alert box. 
+
+// let age=33
+// alert(age)
+
+//2.Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+
+// if (localStorage.getItem("visitCount")) {
+   
+//     let count = localStorage.getItem("visitCount") || 0;
+//    count++;localStorage.setItem("visitCount", count); 
+
+// alert("You have visited this site " + count + " times.");
+
+//3.Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser: 
+
+// let birthYear=1990
+
+// document.write("My birth year is",birthYear+ "<br/> ")
+// document.write("Data type of my declared variable is number.")
+
+//4.. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information: b. Product title c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var customerName = "John Doe"; 
+//         var productTitle = "T-shirt";  
+//         var quantity = 5;            
+//         alert(customerName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
+
+//------------------------------------------------------------------------------------------------------------------
+
+/////////Chapter 4/////////////
+
+
+//1.Declare 3 variables in one statement. 
+//var customerName = "John Doe", productTitle = "T-shirt", quantity = 5;
+
+//2. Declare 5 legal & 5 illegal variable names.
+//LEGAL VARIABLE
+
+// 1.customerName
+//2. product_123
+//3. orderQuantity
+//4. _totalAmount
+// 5.isAvailable
+
+//ILLEGAL VARIABLE
+
+// 1.123product 
+// 2.var (Reserved keyword)
+// 3.product@name 
+// 4.if (Reserved keyword)
+// 5.total-amount 
+
+
+//3. Display this in your browser 
+
+//a)  A heading stating “Rules for naming JS variables” 
+//b)  Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name 
+//d) Variable names are case _________ 
+//e) Variable names should not be JS _________   
+
+// document.write("<h1>Rules of naming JS variables</h1> <br/> Variable names can only contain numbers, $ and _. For example $my_1stVariable <br/> Variables must begin with a letter, $ or _. For example $name, _name or name <br/> Variable names are case sensitive <br/> Variable names should not be JS keywords.")
+
